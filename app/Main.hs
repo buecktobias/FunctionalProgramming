f2938785b375585c3389d48a1e7774fd03afabfb
tobias <buecktobias@gmail.com>
2020-03-28 10:44:59 +0100

    fibonacci numbers

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,10 @@
 module Main where
 
 import Lib
+import Fibonacci
 
 main :: IO ()
 main = do
-  let n = 10
-  let result = fact n
+  let n = 20
+  let result = fib n
   print result
---
+++ src/Fibonacci.hs
@@ -0,0 +1,8 @@
+module Fibonacci(
+fib
+) where
+
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-2) + fib (n-1)
